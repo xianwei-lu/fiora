b6b3caa5214ae8c7a6a8b9ab8671300cd3ea8da7
碎碎酱 <yinxin630@gmail.com>
2017-07-23 21:33:09 +0800

    feat: dynamic loading code editor

--- src/client/features/CodeEditor.jsx
+++ src/client/features/CodeEditor.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { immutableRenderDecorator } from 'react-immutable-render-mixin';
-import AceEditor from 'react-ace';
 import { Modal, Select, Spin } from 'antd';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import 'styles/feature/codeEditor.less';
-import 'brace/theme/tomorrow';
-import 'brace/ext/language_tools';
 
 import action from '../state/action';
 
@@ -28,26 +25,43 @@ const languages = [
     'sql',
     'json',
 ];
+let AceEditor = null;
 
-// 按需加载所选语言的mode文件
-// Load the mode file for the selected language on demand
+// 动态加载所选语言的mode文件
+// Dynamically loads the mode file of the selected language
+let editorLoadEnd = false;
 const langLoadEnd = { };
 function createLanguage(lang, loadFun) {
-    return class Fun extends Component {
+    return class AceEditorWrap extends Component {
         constructor(...args) {
             super(...args);
             this.state = {
-                loadEnd: !!langLoadEnd[lang],
+                editorReady: editorLoadEnd,
+                modeReady: !!langLoadEnd[lang],
             };
         }
         componentDidMount() {
-            if (!this.state.loadEnd) {
+            const { editorReady, modeReady } = this.state;
+            if (!editorReady) {
+                // 动态加载代码编辑器
+                // Dynamic loading code editor
+                require.ensure([], (require) => {
+                    AceEditor = require('react-ace').default;
+                    require('brace/theme/tomorrow');
+                    require('brace/ext/language_tools');
+                    this.setState({ editorReady: true });
+                    loadFun.call(this);
+                    langLoadEnd[lang] = true;
+                }, 'react-ace');
+                editorLoadEnd = true;
+            } else if (!modeReady) {
                 loadFun.call(this);
                 langLoadEnd[lang] = true;
             }
         }
         render() {
-            if (!this.state.loadEnd) {
+            const { editorReady, modeReady } = this.state;
+            if (!editorReady || !modeReady) {
                 return (
                     <Spin tip="loading...">
                         <div className="loadinng" />
@@ -63,85 +77,85 @@ function createLanguage(lang, loadFun) {
 const Javascript = createLanguage('javascript', function () {
     require.ensure([], (require) => {
         require('brace/mode/javascript');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'javascript');
 });
 const Typescript = createLanguage('typescript', function () {
     require.ensure([], (require) => {
         require('brace/mode/typescript');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'typescript');
 });
 const Java = createLanguage('java', function () {
     require.ensure([], (require) => {
         require('brace/mode/java');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'java');
 });
 const Cpp = createLanguage('c_cpp', function () {
     require.ensure([], (require) => {
         require('brace/mode/c_cpp');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'cpp');
 });
 const Python = createLanguage('python', function () {
     require.ensure([], (require) => {
         require('brace/mode/python');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'python');
 });
 const Ruby = createLanguage('ruby', function () {
     require.ensure([], (require) => {
         require('brace/mode/ruby');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'ruby');
 });
 const Php = createLanguage('php', function () {
     require.ensure([], (require) => {
         require('brace/mode/php');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'php');
 });
 const Golang = createLanguage('golang', function () {
     require.ensure([], (require) => {
         require('brace/mode/golang');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'golang');
 });
 const Csharp = createLanguage('csharp', function () {
     require.ensure([], (require) => {
         require('brace/mode/csharp');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'csharp');
 });
 const Html = createLanguage('html', function () {
     require.ensure([], (require) => {
         require('brace/mode/html');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'html');
 });
 const Css = createLanguage('css', function () {
     require.ensure([], (require) => {
         require('brace/mode/css');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'css');
 });
 const Markdown = createLanguage('markdown', function () {
     require.ensure([], (require) => {
         require('brace/mode/markdown');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'markdown');
 });
 const Sql = createLanguage('sql', function () {
     require.ensure([], (require) => {
         require('brace/mode/sql');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'sql');
 });
 const Json = createLanguage('json', function () {
     require.ensure([], (require) => {
         require('brace/mode/json');
-        this.setState({ loadEnd: true });
+        this.setState({ modeReady: true });
     }, 'json');
 });
 
@@ -165,7 +179,6 @@ class CodeEditor extends Component {
         const { value, lang } = this.state;
         const onSend = this.props.onSend;
         if (onSend) {
-            console.log(lang);
             onSend(`!!!lang=${lang}!!!${value}`);
             this.close();
         }
