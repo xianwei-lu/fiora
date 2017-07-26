829ba7f789045f7979c9614806f95706c7409520
碎碎酱 <yinxin630@gmail.com>
2017-07-26 15:09:02 +0800

    feat: add start script.

--- bin/init.js
+++ bin/init.js
@@ -10,24 +10,26 @@ mongoose.Promise = Promise;
 async function init() {
     await mongoose.connect(config.database);
     const group = await Group.findOne({ isDefault: true });
-    if (group) {
-        throw Error('default group already exists.');
+    if (!group) {
+        const defaultGroup = await Group.create({
+            name: 'fiora',
+            announcement: '欢迎各位来到fiora',
+            isDefault: true,
+        });
+        if (!defaultGroup) {
+            throw Error('create default group fail.');
+        }
+        console.log('create default group success.');
+    } else {
+        console.log('default group already exists.');
     }
-
-    const defaultGroup = await Group.create({
-        name: 'fiora',
-        announcement: '欢迎各位来到fiora',
-        isDefault: true,
-    });
-    if (!defaultGroup) {
-        throw Error('create default group fail.');
-    }
-    console.log('create default group success.');
 
     const tempDirectory = path.join(__dirname, '../temp/');
     if (!await fs.exists(tempDirectory)) {
         await fs.mkdir(tempDirectory);
         console.log('create temp directory success.');
+    } else {
+        console.log('temp directory already exist.');
     }
 }
 
